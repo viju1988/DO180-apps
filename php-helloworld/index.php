369ba7dc7cb8e285caac81f83bbfd937951d8f44
Student User <student@workstation.lab.example.com>
2020-09-17 11:06:03 +0000

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, RedHat! php version is " . PHP_VERSION . "\n";
+print "Hello, RedHat! php version is testviju1988 . PHP_VERSION . "\n";
+print "A change is in the air, corona everywhere!\n";
 ?>
